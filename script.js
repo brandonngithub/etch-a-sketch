f779767300f3ea28a240452a4e6e245b6469403c
Brandon Ngo <brandonngo56@gmail.com>
2022-11-16 17:25:03 -0500

    update files

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const container = document.querySelector('#container');
 var cell;
 var target;
 var holdingMouseButton = false;
+var pencilColor = "black";
 
 //makes holdingMouseButton true if so
 document.body.onmousedown = function() {
@@ -30,16 +31,48 @@ function makeGrid(num) {
     }
 }
 
-//change color when mouseover 
-function changeColor(target) {
-    target.style.backgroundColor = 'black';
+//color when click and drag
+function color(target) {
+    target.style.backgroundColor = pencilColor;
 }
 
 container.addEventListener("mouseover", function(e) {
     target = e.target;
     if (target.matches("div.cell") && holdingMouseButton==true) {
-        changeColor(target);
+        color(target);
     }
+});
+
+//change pencil color
+document.getElementById('brown').addEventListener("click", function(e) {
+    pencilColor = "brown";
+});
+document.getElementById('purple').addEventListener("click", function(e) {
+    pencilColor = "purple";
+});
+document.getElementById('red').addEventListener("click", function(e) {
+    pencilColor = "red";
+});
+document.getElementById('orange').addEventListener("click", function(e) {
+    pencilColor = "orange";
+});
+document.getElementById('yellow').addEventListener("click", function(e) {
+    pencilColor = "yellow";
+});
+document.getElementById('green').addEventListener("click", function(e) {
+    pencilColor = "green";
+});
+document.getElementById('blue').addEventListener("click", function(e) {
+    pencilColor = "blue";
+});
+document.getElementById('black').addEventListener("click", function(e) {
+    pencilColor = "black";
+});
+document.getElementById('pink').addEventListener("click", function(e) {
+    pencilColor = "pink";
+});
+document.getElementById('grey').addEventListener("click", function(e) {
+    pencilColor = "grey";
 });
 
 
